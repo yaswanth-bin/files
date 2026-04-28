34f25cbdaadba34b0521b0b7f2932d2a6f8a0bae
Yaswanth <yaswanth.mailbox@gmail.com>
2026-04-28 14:49:29 +0530

    Create validparentheses.java

---
+++ leetcode/java/validparentheses.java
@@ -0,0 +1,29 @@
+//leetcode problem number 20
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for(char ch : s.toCharArray()){
+            if(ch == '(' || ch == '[' || ch == '{'){
+                stack.push(ch);
+            }
+            else{
+                if(stack.isEmpty()){
+                    return false;
+                }
+                char top = stack.pop();
+                if(ch == ')' && top != '('){
+                    return false;
+                }
+                if(ch == ']' && top != '['){
+                    return false;
+                }
+                if(ch == '}' && top != '{'){
+                    return false;
+                }
+         }
+
+        }
+
+        return stack.isEmpty();
+    }
+}
